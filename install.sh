ede864d7031b1b4eab36070545b16bacf7b71d4a
trinityzhub <trinity0091+github@gmail.com>
2023-01-24 02:05:26 +0530

    added VERSION_TAG as User I/p

--- install.sh
+++ install.sh
@@ -31,10 +31,41 @@ CONTAINER_NAME=echo$DOMAIN_NO
 
 
 
+
+choices=( 'latest' 'v0.01' 'v0.02'  ) # sample choices
+select dummy in "${choices[@]}"; do  # present numbered choices to user
+  # Parse ,-separated numbers entered into an array.
+  # Variable $REPLY contains whatever the user entered.
+  IFS=', ' read -ra selChoices <<<"$REPLY"
+  # Loop over all numbers entered.
+  for choice in "${selChoices[@]}"; do
+    # Validate the number entered.
+    (( choice >= 1 && choice <= ${#choices[@]} )) || { echo "Invalid choice: $choice. Try again." >&2; continue 2; }
+    # If valid, echo the choice and its number.
+    echo "Choice #$(( ++choiceIncrementor )): ${choices[choice-1]} ($choice)"
+    mychoice==${choices[choice-1]}
+  done
+  # All choices are valid, exit the prompt.
+  break
+done
+
+
+
+
+VERSION_TAG=$mychoice
+
+
 echo $1
 
 docker build  . -t test$DOMAIN_NO/hello:v0.01 -f $1
 
 
-docker run -d --name $CONTAINER_NAME--rm test-$DOMAIN_NO/hello:v0.01
+docker run -d --name $CONTAINER_NAME--rm test-$DOMAIN_NO/hello:$VERSION_TAG
 
+
+
+unset choices
+unset selChoices
+unset choiceIncrementor
+unset dummy
+unset mychoice
